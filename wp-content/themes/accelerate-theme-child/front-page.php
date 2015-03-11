fa17f1870ec32f8630499d62c5a90a18a7a07235
christineoskison <Christine@Darins-iMac.local>
2015-03-11 19:02:59 -0400

    update front page and style.css 3.11.15

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -66,11 +66,5 @@ get_header(); ?>
     </div>
 </section>
 
-<?php if ( is_active_sidebar( 'sidebar-2' ) ) : ?>
-<div id="secondary" class="widget-area" role="complementary">
-	<?php dynamic_sidebar( 'sidebar-2' ); ?>
-</div>
-<?php endif; ?>
-
 
 <?php get_footer(); ?>
